bf7819c0d922580505f0c5d47d3f385d92010d4b
heyyou <hey3you3@gmail.com>
2021-10-30 02:21:22 +0900

    add: Command Domain Getter

--- domain/command/command.go
+++ domain/command/command.go
@@ -5,3 +5,15 @@ type Command struct {
 	description    string
 	tags           []string
 }
+
+func (c *Command) OnelineCommand() string {
+	return c.onelineCommand
+}
+
+func (c *Command) Description() string {
+	return c.description
+}
+
+func (c *Command) Tags() []string {
+	return c.tags
+}
